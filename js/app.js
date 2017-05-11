0ff73009b8f2f3f7c37bfd0f0c31db88ee6cbe2c
Julio Lima <julio_flima@hotmail.com>
2017-05-11 07:39:53 -0300

    insert pre in the index

--- js/app.js
+++ js/app.js
@@ -18,4 +18,4 @@
     //Autentication Tomate Store
     authTomateStore();
 
-}());
+}())
--- js/db.js
+++ js/db.js
@@ -1,16 +1,13 @@
 (function dbTomateStore() {
 
-
-    console.log("initing");
-
     //Get elements
     const preObject = document.getElementById('object');
-
 
     //Create references
     const dbRefObject = firebase.database().ref().child('object');
 
     //Sync object changes
-    dbRefObject.on('value', snap => console.log(snap.val()));
-
+    dbRefObject.on('value', snap => {
+       preObject.innerHTML = JSON.stringify(snap.val(), null, 3);
+    });
 }())
