e32a33a1375c22cff0ab3bf7b2fa55e0a0a92312
Julio Lima <julio_flima@hotmail.com>
2017-05-11 10:14:35 -0300

    database tes 2

--- js/db.js
+++ js/db.js
@@ -5,9 +5,12 @@
 
     //Create references
     const dbRefObject = firebase.database().ref().child('object');
+    const dbRefObject = firebase.database().ref().child('hobbies')
 
     //Sync object changes
     dbRefObject.on('value', snap => {
        preObject.innerHTML = JSON.stringify(snap.val(), null, 3);
     });
+
+    bdRefList.on('child_added', snap => console.log(snap.val()));
 }())
