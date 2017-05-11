ee358981dc1d170a3f6cab6d8dc9af1b2b27bc02
Melisse Cabral <Melisse Cabral>
2017-05-11 13:19:55 -0300

    refactoring app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,16 @@
 (function () {
 
     'use strict';
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB9Ro3b5hTFZTpwa2e4I-Ltxga5rsFx2hA",
-    authDomain: "tomatestore-40234.firebaseapp.com",
-    databaseURL: "https://tomatestore-40234.firebaseio.com",
-    projectId: "tomatestore-40234",
-    storageBucket: "tomatestore-40234.appspot.com",
-    messagingSenderId: "193843683809"
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB9Ro3b5hTFZTpwa2e4I-Ltxga5rsFx2hA",
+        authDomain: "tomatestore-40234.firebaseapp.com",
+        databaseURL: "https://tomatestore-40234.firebaseio.com",
+        projectId: "tomatestore-40234",
+        storageBucket: "tomatestore-40234.appspot.com",
+        messagingSenderId: "193843683809"
+    };
 
-    //Database Tomate Store
-    dbTomateStore();
-
-    //Autentication Tomate Store
-    authTomateStore();
+    firebase.initializeApp(config);
 
 }())
