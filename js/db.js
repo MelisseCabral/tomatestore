abd3233465044d9dfc90da1ed621929a1d41207c
Julio Lima <julio_flima@hotmail.com>
2017-05-11 00:13:05 -0300

    deleting auth

--- js/db.js
+++ js/db.js
@@ -1,3 +1,16 @@
 (function dbTomateStore() {
-    alert("Amorrrr!!!");
-}())
+
+
+    console.log("initing");
+
+    //Get elements
+    const preObject = document.getElementById('object');
+
+
+    //Create references
+    const dbRefObject = firebase.database().ref().child('Object');
+
+    //Sync object changes
+    dbRefObject.on('value', snap => console.log(snap.val()));
+
+}());
